4b8a47b3ea4ac9860c63e27dc5368f9dbeba2e4c
Yongle Yuan <yongle.jack.yuan@gmail.com>
2024-12-16 23:18:53 -0500

    Sketchybar not draw wechat/mail when not init

--- .config/sketchybar/plugins/mail.sh
+++ .config/sketchybar/plugins/mail.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-COUNT=$($HOME/.config/$BAR_NAME/scripts/notification.sh Mail)
-THRES=1
+OPENED=$(ps aux | grep -v grep | grep -ci "MacOS/Mail")
 
-if [[ $COUNT -lt $THRES ]]; then
-  $BAR_NAME --set $NAME label.drawing=off icon.padding_right=8
+if [[ $OPENED -eq 0 ]]; then
+  $BAR_NAME --set $NAME drawing=off
+
 else
-  $BAR_NAME --set $NAME label.drawing=on label="${COUNT}" icon.padding_right=3
+  $BAR_NAME --set $NAME drawing=on
+
+  COUNT=$($HOME/.config/$BAR_NAME/scripts/notification.sh Mail)
+
+  if [[ $COUNT -lt 1 ]]; then
+    $BAR_NAME --set $NAME label.drawing=off icon.padding_right=8
+  else
+    $BAR_NAME --set $NAME label.drawing=on label="${COUNT}" icon.padding_right=3
+  fi
 fi
 
